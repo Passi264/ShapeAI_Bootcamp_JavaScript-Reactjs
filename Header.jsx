33ef1ae202a20f2c4310be47e842061f94907998
Passi264 <84937084+Passi264@users.noreply.github.com>
2021-06-06 01:38:51 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>MyProject</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>TicTacToe</h1>
+      <p>
+        This project includes a game made using c++ and the game itself is very responsive.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
